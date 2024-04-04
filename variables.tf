96a6cd1bfe4da36ffc1ba76fffca17a2d7b4756b
Barny <barnybaron-aws@yahoo.com>
2024-04-04 21:44:53 +0100

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  description = "This is the bucket name for S3."
+  default = "bb-first-terraform-bucket-354"
+}
+
+variable "website_index_document" {
+  description = "This is the website index document."
+  default = "index.html"
+}
